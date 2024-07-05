beb92d76b6953884ca1cdb3a8598e32153687076
aoilunasky <aoilunasky1995@gmail.com>
2024-07-05 21:40:28 +0900

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,7 +6,6 @@ use App\Models\Package;
 use App\Models\Student;
 use App\Models\Subject;
 use App\Models\Teacher;
-use Database\Factories\SubjectFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -22,19 +21,19 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             CountrySeeder::class,
             LevelSeeder::class,
-            PackageSeeder::class
+            PackageSeeder::class,
         ]);
         Subject::factory(4)->create();
         Teacher::factory(20)->create();
         Student::factory(20)->create();
         $subjects = Subject::all();
-        Teacher::all()->each(function ($teacher) use ($subjects){
+        Teacher::all()->each(function ($teacher) use ($subjects) {
             $teacher->subjects()->attach(
                 $subjects->random(rand(1, 3))->pluck('id')->toArray()
             );
         });
         $package = Package::find(1);
-        Student::all()->each(function ($studnet) use ($package){
+        Student::all()->each(function ($studnet) use ($package) {
             $studnet->packages()->attach($package->id,
                 [
                     'date' => now(),
@@ -42,7 +41,7 @@ class DatabaseSeeder extends Seeder
                     'ticket_amt' => $package->ticket_amount,
                     'fees' => $package->fees,
                     'created_at' => now(),
-                    'updated_at' => now()
+                    'updated_at' => now(),
                 ]
             );
             $studnet->ticket_amt = $package->ticket_amount;
