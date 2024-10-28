3a8c43bcf7da171bde6a65b03d19acff5d6a3bd2
aoilunasky <aoilunasky1995@gmail.com>
2024-10-28 13:05:03 +0900

    pusher update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import Vue from "@vitejs/plugin-vue"; // Add here
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
+        Vue(),
     ],
 });
